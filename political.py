e684d42755e828860e4bec54da270f2596cf6d32
Peter Madsen <cephas.ryle@gmail.com>
2015-07-25 23:53:13 -0700

    Add joined Race class to political

--- political.py
+++ political.py
@@ -24,29 +24,9 @@ class Race:
     def __str__(self):
         return self.race_name + " " + self.county
 
-class ComboRace(Race):
-
-    def __init__(self, f, directory):
-        self.f = f
-        self.directory = directory
-        super(ComboRace, self).__init__(self.f[:-6], self.f[-6:-4])
-        self.candidates = self.get_candidates()
-
-    def get_candidates(self):
-        import csv
-        import os
-
-        non_candidates = ['Row Label', 'County']
-
-        with open(os.path.join(self.directory, self.f), 'r') as results_file:
-            results_read = csv.DictReader(results_file, delimiter=',')
-
-            try:
-                columns = results_read.next()
-                # print (type(columns))
-            except StopIteration:
-                print(self.race_name)
-
-            for value in columns:
-                if value not in non_candidates:
-                    self.candidates.append(value)
+class JoinedRace(Race):
+    """docstring for JoinedRace"""
+    def __init__(self, race_name, candidates, rows_to_write):
+        super(JoinedRace, self).__init__(race_name, race_name[:-2])
+        self.race_name = race_name
+        
